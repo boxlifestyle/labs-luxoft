6ae2d3c5250f9ee7579fb2cbe6c37c292bf2e976
Bykov P <pbykov@neoflex.ru>
2021-11-11 06:14:23 +0300

    lab5

--- lab-5-aop/src/main/java/com/luxoft/springaop/lab5/aspects/LoggingAspect.java
+++ lab-5-aop/src/main/java/com/luxoft/springaop/lab5/aspects/LoggingAspect.java
@@ -6,8 +6,10 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.core.annotation.Order;
 
 @Aspect
+@Order(1)
 public class LoggingAspect {
 	private final static Logger LOG = Logger.getLogger(LoggingAspect.class.getName());
 
