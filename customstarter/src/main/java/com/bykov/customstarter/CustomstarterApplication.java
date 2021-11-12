99214468cafdc2f2fec6318e52eaedabf5b3881d
Bykov P <pbykov@neoflex.ru>
2021-11-12 08:42:30 +0300

    Быков Павел

---
+++ customstarter/src/main/java/com/bykov/customstarter/CustomstarterApplication.java
@@ -0,0 +1,13 @@
+package com.bykov.customstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomstarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomstarterApplication.class, args);
+	}
+
+}
---
+++ customstarter/src/test/java/com/bykov/customstarter/CustomstarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bykov.customstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomstarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
