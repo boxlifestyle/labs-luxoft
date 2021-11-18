4b6a4167cf75b7d8c3966582c5198a4e04195865
Bykov P <pbykov@neoflex.ru>
2021-11-18 10:02:18 +0300

    Быков Павел

---
+++ testspec/src/main/java/com/bykov/testspec/TestspecApplication.java
@@ -0,0 +1,13 @@
+package com.bykov.testspec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestspecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestspecApplication.class, args);
+	}
+
+}
